1ec0f6959d92311e173b37a8958ddb3e16e7b0c5
Ben Rasmusen <mail@benrasmusen.com>
2012-08-22 14:40:33 -0400

    Add custom template for nextgen-gallery plugin to allow for custom markup on galleries.

---
+++ wp-content/plugins/nextgen-gallery/view/gallery-ash-custom.php
@@ -0,0 +1,26 @@
+<?php 
+/**
+ * Custom template for the Ash Center theme
+ **/
+?>
+<?php if (!defined ('ABSPATH')) die ('No direct access allowed'); ?><?php if (!empty ($gallery)) : ?>
+	
+<section class="box2">
+	<a class="previous-btn disabled"> </a> <a class="next-btn"> </a>
+	<div class="inner">
+		<ul class="clearfix">
+			<?php foreach ( $images as $image ) : ?>
+			<li>
+				<a href="<?php echo $image->imageURL ?>" title="<?php echo $image->description ?>" rel="prettyPhoto{gallery}">
+					<img title="<?php echo $image->alttext ?>" alt="<?php echo $image->alttext ?>" src="<?php echo $image->thumbnailURL ?>" <?php echo $image->size ?> />
+				</a>
+				<?php if (!empty($image->alttext)): ?>
+					<h3><?php echo $image->alttext ?></h3>
+				<?php endif ?>
+			</li>
+			<?php endforeach; ?>
+		</ul>
+	</div>
+</section>
+
+<?php endif; ?>

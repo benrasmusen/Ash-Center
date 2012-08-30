1555ec9deacc63de38adec982de3cadb354b6b7f
Ben Rasmusen <mail@benrasmusen.com>
2012-08-30 13:14:41 -0400

    Minor modification to the custom template for the nextgen-gallery plugin to allow separation of modal slideshow.

--- wp-content/plugins/nextgen-gallery/view/gallery-ash-custom.php
+++ wp-content/plugins/nextgen-gallery/view/gallery-ash-custom.php
@@ -4,14 +4,14 @@
  **/
 ?>
 <?php if (!defined ('ABSPATH')) die ('No direct access allowed'); ?><?php if (!empty ($gallery)) : ?>
-	
+
 <section class="box2">
 	<a class="previous-btn disabled"> </a> <a class="next-btn"> </a>
 	<div class="inner">
 		<ul class="clearfix">
 			<?php foreach ( $images as $image ) : ?>
 			<li>
-				<a href="<?php echo $image->imageURL ?>" title="<?php echo $image->description ?>" rel="prettyPhoto{gallery}">
+				<a href="<?php echo $image->imageURL ?>" title="<?php echo $image->description ?>" rel="prettyPhoto{<?php echo $gallery->name ?>}">
 					<img title="<?php echo $image->alttext ?>" alt="<?php echo $image->alttext ?>" src="<?php echo $image->thumbnailURL ?>" <?php echo $image->size ?> />
 				</a>
 				<?php if (!empty($image->alttext)): ?>
